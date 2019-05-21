91815732356766ee59d3aa66a0cbf5ef0e4a0d1c
Tony Cheng <shilmista@gmail.com>
2019-05-21 12:45:11 -0400

    finished problem

--- may-5.py
+++ may-5.py
@@ -7,3 +7,18 @@
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 # Bonus: Can you do this in one pass?
+
+def any_two_numbers(array, target):
+    numbers = set()
+    for num in array:
+        test_num = target - num
+        if test_num in numbers:
+            return (test_num, num)
+        else:
+            numbers.add(num)
+    return False
+
+
+array = [10, 15, 3, 7]
+target = 17
+print(any_two_numbers(array, target))
